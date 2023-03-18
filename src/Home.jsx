7a80e46183b1d719ffc4e60db91e23533e605e01
Anany Agrawal <ananyagrawal698@gmail.com>
2023-03-18 20:17:27 +0530

    import all sections

--- src/Home.jsx
+++ src/Home.jsx
@@ -5,6 +5,8 @@ import HomeSection2 from "./home sections/HomeSection2";
 import HomeSection3 from "./home sections/HomeSection3";
 import HomeSection4 from "./home sections/HomeSection4";
 import HomeSection5 from "./home sections/HomeSection5";
+import HomeSection6 from "./home sections/HomeSection6";
+import HomeSection7 from "./home sections/HomeSection7";
 const Home = () => {
   return (
     <>
@@ -16,6 +18,8 @@ const Home = () => {
       <HomeSection3 />
       <HomeSection4 />
       <HomeSection5 />
+      <HomeSection6 />
+      <HomeSection7 />
     </>
   );
 };
