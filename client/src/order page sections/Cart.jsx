3220c7954c45e70e1c39a19195adadb1eb3ab59e
Anany Agrawal <ananyagrawal698@gmail.com>
2023-04-11 14:26:09 +0530

    modify cart frontend

--- client/src/order page sections/Cart.jsx
+++ client/src/order page sections/Cart.jsx
@@ -1,10 +1,18 @@
 import styles from "./Cart.module.css";
+import { IoMdClose } from "react-icons/io";
 const Cart = () => {
   return (
     <div className={styles.cart_container}>
       <div className={styles.cart_content}>
-        <h1>Your Order</h1>
-        <p>2 items</p>
+        <div className={styles.top_section}>
+          <div className={styles.top_section_left}>
+            <h1>YOUR ORDER</h1>
+            <p>2 items</p>
+          </div>
+          <div className={styles.top_section_right}>
+            <IoMdClose size={20} />
+          </div>
+        </div>
       </div>
     </div>
   );
